bd294d90ff2e296cd9c316bd9392cf69c047eebd
Teo-Te <52335032+Teo-Te@users.noreply.github.com>
2024-04-17 16:57:10 +0200

    second miniproject, price grids

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,8 @@ export default function Home() {
   return (
     <div>
       <h1>TailwindCSS Training</h1>
-      <Link href="/emailsub">Fut e bjer jqj u merzitem</Link>
+      <Link href="/emailSub">Fut e bjer jqj u merzitem</Link>
+      <Link href="/priceGrids">Fut e bjer jqj u merzitem</Link>
     </div>
   );
 }
